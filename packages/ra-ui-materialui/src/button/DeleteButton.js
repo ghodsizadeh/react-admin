1436d1625bc30f75af9831501284db89b058a279
Gildas Garcia <gildas.garcia@pm.me>
2019-03-09 14:43:49 +0100

    Prepare ra-ui-materialui Migration to TypeScript
    I locally added the `checkJs` option to the package `tsconfig.json` file (not commited). This gives us the opportunity to fix many issues before starting the migration to TS.
    
    - [x] Use `createStyles`
    - [x] Check `isValidElement` before cloning where necessary
    - [x] Use `Children.only` before cloning when necessary
    - [ ] Find a TypeScript compatible way to sanitize props
    - [ ] Fix all other possible errors

--- packages/ra-ui-materialui/src/button/DeleteButton.js
+++ packages/ra-ui-materialui/src/button/DeleteButton.js
@@ -1,14 +1,94 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import DeleteWithUndoButton from './DeleteWithUndoButton';
-import DeleteWithConfirmButton from './DeleteWithConfirmButton';
+import { connect } from 'react-redux';
+import compose from 'recompose/compose';
+import { withStyles, createStyles } from '@material-ui/core/styles';
+import { fade } from '@material-ui/core/styles/colorManipulator';
+import ActionDelete from '@material-ui/icons/Delete';
+import classnames from 'classnames';
+import { translate, crudDelete, startUndoable } from 'ra-core';
 
-const DeleteButton = ({ undoable, ...props }) =>
-    undoable ? (
-        <DeleteWithUndoButton {...props} />
-    ) : (
-        <DeleteWithConfirmButton {...props} />
-    );
+import Button from './Button';
+
+const styles = theme => createStyles({
+    deleteButton: {
+        color: theme.palette.error.main,
+        '&:hover': {
+            backgroundColor: fade(theme.palette.error.main, 0.12),
+            // Reset on mouse devices
+            '@media (hover: none)': {
+                backgroundColor: 'transparent',
+            },
+        },
+    },
+});
+
+const sanitizeRestProps = ({
+    basePath,
+    classes,
+    dispatchCrudDelete,
+    filterValues,
+    label,
+    resource,
+    selectedIds,
+    startUndoable,
+    undoable,
+    redirect,
+    ...rest
+}) => rest;
+
+class DeleteButton extends Component {
+    handleDelete = event => {
+        event.stopPropagation();
+        const {
+            dispatchCrudDelete,
+            startUndoable,
+            resource,
+            record,
+            basePath,
+            redirect,
+            undoable,
+            onClick,
+        } = this.props;
+        if (undoable) {
+            startUndoable(
+                crudDelete(resource, record.id, record, basePath, redirect)
+            );
+        } else {
+            dispatchCrudDelete(resource, record.id, record, basePath, redirect);
+        }
+
+        if (typeof onClick === 'function') {
+            onClick();
+        }
+    };
+
+    render() {
+        const {
+            label = 'ra.action.delete',
+            classes = {},
+            className,
+            icon,
+            onClick,
+            ...rest
+        } = this.props;
+        return (
+            <Button
+                onClick={this.handleDelete}
+                label={label}
+                className={classnames(
+                    'ra-delete-button',
+                    classes.deleteButton,
+                    className
+                )}
+                key="button"
+                {...sanitizeRestProps(rest)}
+            >
+                {icon}
+            </Button>
+        );
+    }
+}
 
 DeleteButton.propTypes = {
     basePath: PropTypes.string,
